3bbb8841d5aac26b971794d231e60e39faa1a1bf
Jacob Colvin <jacobcolvin1@gmail.com>
2020-08-19 20:44:31 -0400

    Add X axis labels to graph

--- update_graph.sh
+++ update_graph.sh
@@ -8,7 +8,18 @@ GRAPH=$(prometheus_ascii \
 	--graph.height="12" \
 	--query.duration="168h")
 
-echo -e "\n\`\`\`\n$GRAPH\n\`\`\`\n" >insert_file
+# Store generated content in a file to avoid problems with sed and special characters.
+touch insert_file
+
+echo -e "\n\`\`\`" >>insert_file
+
+echo -e "$GRAPH" >>insert_file
+
+# Append X axis labels. TODO: add support to asciigraph
+echo -e "            ┼─────────────┬─────────────┬─────────────┬─────────────┬─────────────┬─────────────┬──────────────┤ " >>insert_file
+echo -e "           -7d           -6d           -5d           -4d           -3d           -2d           -1d            now" >>insert_file
+
+echo -e "\`\`\`\n" >>insert_file
 
 COMMENT_START="<!-- START_SECTION:ascii_graph -->"
 COMMENT_END="<!-- END_SECTION:ascii_graph -->"
